54963370a4a90a55160a37caf3b020f9c082e045
Bret Young <hby@acm.org>
2016-01-07 15:53:04 -0600

    Both front and backend logic fully parameterized on message type. Seperation of low level message transport and higher lever message behavior.

--- src-cljs/gallows/core.cljs
+++ src-cljs/gallows/core.cljs
@@ -12,6 +12,17 @@
   )
 
 (defonce messages (atom []))
+(defonce player (atom ""))
+(defonce game (atom {:role nil  ; :guesser or :worder
+                     :other nil ; "other player name"
+                     }))
+(defonce players (atom []))
+
+(defn send-ws-message
+  [type payload]
+  (ws/send-transit-msg!
+    {:type type
+     :payload payload}))
 
 (defn message-list []
   [:ul
@@ -24,19 +35,19 @@
     (fn []
       [:input.form-control
        {:type :text
-        :placeholder "type in a message and press enter"
+        :placeholder "type in the name to want to use and press enter"
         :value @value
         :on-change #(reset! value (-> % .-target .-value))
         :on-key-down #(when (= (.-keyCode %) 13)
-                       (ws/send-transit-msg!
-                         {:message @value})
+                       (send-ws-message :new-message
+                                        {:message @value})
                        (reset! value nil))}])))
 
 (defn home-page []
   [:div.container
    [:div.row
     [:div.col-md-12
-     [:h2 "Welcome to chat"]]]
+     [:h2 "Who are you?"]]]
    [:div.row
     [:div.col-sm-6
      [message-list]]]
@@ -44,12 +55,33 @@
     [:div.col-sm-6
      [message-input]]]])
 
+
+;;; ws messages ;;;
+
+;;
+;; :new-message
+;;
 (defn update-messages! [{:keys [message]}]
   (swap! messages #(vec (drop (- (count @messages) 10) (conj % message)))))
+
+;;
+;; :new-player
+;;
+(defn update-player-list! [{:keys [player]}]
+  )
+
+;;; mount components and set up ws connection ;;;
+
+(defn receive-ws-message
+  [{:keys [type payload]}]
+  (do
+    (println "got ws message type:" type "payload:" payload)
+    (case type
+      :new-message (update-messages! payload))))
 
 (defn mount-components []
   (reagent/render-component [#'home-page] (.getElementById js/document "app")))
 
 (defn init! []
-  (ws/make-websocket! (str "ws://" (.-host js/location) "/ws") update-messages!)
+  (ws/make-websocket! (str "ws://" (.-host js/location) "/ws") receive-ws-message)
   (mount-components))
--- src/gallows/routes/websockets.clj
+++ src/gallows/routes/websockets.clj
@@ -3,9 +3,77 @@
             [org.httpkit.server
              :refer [send! with-channel on-close on-receive]]
             [cognitect.transit :as t]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log])
+  (:import (java.io ByteArrayInputStream ByteArrayOutputStream)))
 
 (defonce channels (atom #{}))
+
+(defn transit-read
+  "return Cloure data given transit data"
+  [tmsg]
+  (let [in (ByteArrayInputStream. (.getBytes tmsg))
+        reader (t/reader in :json)
+        msg (t/read reader)]
+    msg))
+
+(defn transit-write
+  "return transit data given Clojure data"
+  [msg]
+  (let [out (ByteArrayOutputStream. 4096)
+        writer (t/writer out :json)
+        _ (t/write writer msg)]
+    (.toString out)))
+
+(defn send-ws-message
+  "msg is encoded as transit data and sent to channels
+   channels - web socket channels
+   msg     - Clojure data to send"
+  [msg & channels]
+  (let [tmsg (transit-write msg)]
+    (doseq [channel channels]
+      (send! channel tmsg))))
+
+
+(defn notify-all-clients [msg]
+  (do
+    (log/info "notifying all clients:" msg)
+    (apply send-ws-message msg @channels)))
+
+(defn ->message
+  "convenince web socket message creator"
+  [type payload]
+  {:type type
+   :payload payload})
+
+
+;;; ws messages ;;;
+
+;;
+;; :new-message
+;;
+(defn new-message
+  [payload]
+  (notify-all-clients (->message :new-message payload)))
+
+;;
+;; :new-player
+;;
+(defn new-player [{:keys [player]}]
+  )
+
+
+
+;;; Web Socket connect, disconnect, and raw handler
+
+(defn handle-ws-message
+  "pass payload to function determined by (msg :type)"
+  [{:keys [type payload] :as msg}]
+  (do
+    (log/info "handling ws message:" msg)
+    (case type
+      :new-message (new-message payload)
+      :new-player (new-player payload)
+      )))
 
 (defn connect! [channel]
   (log/info "channel open:" channel)
@@ -15,16 +83,11 @@
   (log/info "channel closed:" status channel)
   (swap! channels #(remove #{channel} %)))
 
-(defn notify-clients [msg]
-  (doseq [channel @channels]
-    (log/info "notify-clients:" msg)
-    (send! channel msg)))
-
 (defn ws-handler [request]
   (with-channel request channel
                 (connect! channel)
                 (on-close channel (partial disconnect! channel))
-                (on-receive channel #(notify-clients %))))
+                (on-receive channel #(handle-ws-message (transit-read %)))))
 
 (defroutes websocket-routes
            (GET "/ws" request (ws-handler request)))
